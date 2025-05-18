323ac15be43da0522239461bfeef4b776e3d3f25
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-05-18 11:01:05 -0300

    :sparkles: feat: Author model

---
+++ src/main/java/edu/br/ifpr/model/entity/Author.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.br.ifpr.model.entity;
+
+/**
+ *
+ * @author rafael
+ */
+public class Author {
+
+    private int author_id;
+    private String name;
+
+    public Author() {
+    }
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public int getAuthor_id() {
+        return author_id;
+    }
+
+    public void setAuthor_id(int author_id) {
+        this.author_id = author_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" + "author_id=" + author_id + ", name=" + name + '}';
+    }
+
+}
