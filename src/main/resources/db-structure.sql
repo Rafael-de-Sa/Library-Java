9d56d31d8dd270cb3067b530d7dc0cf5ad767ee2
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-05-18 12:57:47 -0300

    :bug: fix: author_id and book_id AUTO INCREMENT

--- src/main/resources/db-structure.sql
+++ src/main/resources/db-structure.sql
@@ -23,7 +23,7 @@ DROP TABLE IF EXISTS `authors`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `authors` (
-  `author_id` int NOT NULL,
+  `author_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`author_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
@@ -37,7 +37,7 @@ DROP TABLE IF EXISTS `books`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `books` (
-  `book_id` int NOT NULL,
+  `book_id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `pages` int NOT NULL,
   `author_id` int NOT NULL,
@@ -60,4 +60,4 @@ CREATE TABLE `books` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-05-18 12:19:36
+-- Dump completed on 2025-05-18 12:54:42
