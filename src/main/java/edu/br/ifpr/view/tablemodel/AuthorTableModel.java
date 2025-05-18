2fd267666a6f91253cd85880b98feed7612bae7e
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-05-18 13:43:28 -0300

    :sparkles: feat: AuthorTableModel

---
+++ src/main/java/edu/br/ifpr/view/tablemodel/AuthorTableModel.java
@@ -0,0 +1,69 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.br.ifpr.view.tablemodel;
+
+import edu.br.ifpr.model.entity.Author;
+import java.util.LinkedList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author rafael
+ */
+public class AuthorTableModel extends AbstractTableModel {
+
+    private List<Author> data = new LinkedList<>();
+
+    private String[] columns = {"ID", "Nome"};
+
+    public AuthorTableModel() {
+    }
+
+    public Author get(int row) {
+        return data.get(row);
+    }
+
+    public void add(Author a) {
+        this.data.add(a);
+        this.fireTableDataChanged();
+    }
+
+    public void remove(int row) {
+        data.remove(row);
+        this.fireTableDataChanged();
+    }
+
+    @Override
+    public int getRowCount() {
+        return data.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return columns.length;
+    }
+
+    @Override
+    public String getColumnName(int column) {
+        return columns[column];
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Author author = data.get(rowIndex);
+
+        switch (columnIndex) {
+            case 1:
+                return author.getAuthor_id();
+            case 2:
+                return author.getName();
+            default:
+                return null;
+        }
+
+    }
+
+}
