6a6be450f10ed9b02f8415bd19b172dd9d005d2e
Rafael de Sá <rafaeldesa170622@gmail.com>
2025-05-18 13:06:57 -0300

    :test_tube: test: create author test

--- src/main/java/edu/br/ifpr/LibraryProject.java
+++ src/main/java/edu/br/ifpr/LibraryProject.java
@@ -1,16 +1,23 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package edu.br.ifpr;
+
+import edu.br.ifpr.dao.AuthorDao;
+import edu.br.ifpr.model.entity.Author;
 
 /**
  *
  * @author rafael
  */
 public class LibraryProject {
-
+    
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        Author author = new Author("Clarice Lispector");
+        AuthorDao authorDao = new AuthorDao();
+        
+        authorDao.create(author);
+        
     }
 }
